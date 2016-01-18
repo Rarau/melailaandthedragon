1e48ce9ce667cc87dd427ea78963082e07786055
Raul <rarau001@campus.goldsmiths.ac.uk>
2016-01-18 21:08:44 +0100

    Minor changes to start testing

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = - target.position + transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.position + offset;
+	}
+}
