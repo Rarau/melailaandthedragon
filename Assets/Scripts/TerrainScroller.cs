f1104748bd9177373c5c182afb0c840314cef7a7
Tin-Tin <kt-chin@hotmail.com>
2016-01-26 19:42:09 +0000

    SCROLLING BACKGROOOUUUNNNDDDSSS

---
+++ Assets/Scripts/TerrainScroller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainScroller : MonoBehaviour {
+
+    private float scrollSpeed = 0.1f;
+    public float offSet;
+    public Renderer LeftWall;
+    public Renderer RightWall;
+    public Renderer Floor;
+    // Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        offSet = Time.time * scrollSpeed;
+        LeftWall.material.SetTextureOffset("_MainTex", new Vector2(offSet, 0));
+        RightWall.material.SetTextureOffset("_MainTex", new Vector2(-offSet, 0));
+        Floor.material.SetTextureOffset("_MainTex", new Vector2(0,-offSet));
+    }
+}
