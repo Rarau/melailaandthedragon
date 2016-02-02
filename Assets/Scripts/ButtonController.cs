c8e9d6166747cacdffa7cfbfb3812fa4f3bc335c
kt-chin <kt-chin@hotmail.com>
2016-02-02 14:37:09 +0000

    Testing out enemy pooling

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -1,23 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Linq;
+using System.Collections.Generic;
 
 public class ButtonController : MonoBehaviour {
     public GameObject[] buttons;
-    
-   // public ReelController[] reels;
+    public GameObject slime;
+    public GameObject[] enemyType;
+    public int enemyAmount = 5;
+    List<GameObject> enemies;
+
+
     public Camera camera; //create camera object to affect the intended camera for raycast, else Camera.Main finds first main camera.
     Ray ray;
     RaycastHit rayHit;
 	// Use this for initialization
 	void Start () {
         buttons = GameObject.FindGameObjectsWithTag("buttons").OrderBy(go => go.name).ToArray(); // Sorts array of gameobjects in order by name. requires System.Linq
+
+        //enemies = new List<GameObject>();
+        //for (int i = 0; i< enemyAmount;i++)
+        //{
+        //    GameObject obj = (GameObject)Instantiate(slime);
+        //    obj.SetActive(false);
+        //    enemies.Add(obj);
+        //}                                                     //Testing out pooling
+
        // HandleController reel = gameObject.GetComponent<HandleController>();
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    void Update ()
     {
+
+
+        if (Input.GetKeyDown("x"))
+        {
+            GameObject enemy = GameObject.FindGameObjectWithTag("Slime");
+            Destroy(enemy);
+        }
+        //if (GameObject.FindGameObjectsWithTag("Slime") == null)
+        //{
+        //    Instantiate()
+        //}
         HandleController HandleScript = FindObjectOfType<HandleController>();
         ReelController ReelScript = FindObjectOfType<ReelController>();
         ray = camera.ScreenPointToRay(Input.mousePosition);
